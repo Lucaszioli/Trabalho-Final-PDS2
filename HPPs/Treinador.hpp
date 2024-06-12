d5243675c3ec4b349aba732900b1074e7fc4c0ea
Henrique Dias <henriquejnd@proton.me>
2024-06-12 15:59:57 -0300

    refactor: Atualiza classe Treinador para deixar como classe pai de bot e jogador

--- HPPs/Treinador.hpp
+++ HPPs/Treinador.hpp
@@ -1,22 +1,24 @@
+#ifndef TREINADOR_HPP
+#define TREINADOR_HPP
+
 #include <string>
 #include <vector>
 #include "Monstrinho.hpp"
 
 class Treinador
 {
-private:
-    std::string falas;
-
 protected:
     int ID;
     vector<Monstrinho *> equipe;
     std::string nome;
 
 public:
-    Treinador(int ID, std::string nome, vector<Monstrinho *> equipe, std::string falas);
+    Treinador(int ID, std::string nome, vector<Monstrinho *> equipe);
     int getID();
     std::string getNome();
     vector<Monstrinho *> getEquipe();
-    std::string getFalas();
-    virtual bool verificaEquipe();
+    bool verificaEquipe();
+    virtual void mudaEquipe();
 };
+
+#endif
