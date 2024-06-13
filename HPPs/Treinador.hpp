b75d2d02a7e44e42c1cdfaa67e06f5b01902e8ad
Henrique Dias <henriquejnd@proton.me>
2024-06-13 11:39:07 -0300

    refactor: Atualiza comentários para estilo doxygen

--- HPPs/Treinador.hpp
+++ HPPs/Treinador.hpp
@@ -5,20 +5,70 @@
 #include <vector>
 #include "Monstrinho.hpp"
 
+/**
+ * @class Treinador
+ *
+ * @brief Classe que representa um treinador de monstrinhos.
+ *
+ * A classe Treinador possui um identificador, um nome e uma equipe de monstrinhos.
+ * Ela fornece métodos para obter o identificador, o nome e a equipe do treinador,
+ * verificar se a equipe está completa, mudar a equipe e imprimir a equipe.
+ */
 class Treinador
 {
 protected:
-    int ID;
-    vector<Monstrinho *> equipe;
-    std::string nome;
+    int ID;                      /**< Identificador do treinador */
+    vector<Monstrinho *> equipe; /**< Equipe de monstrinhos do treinador */
+    std::string nome;            /**< Nome do treinador */
 
 public:
+    /**
+     * @brief Construtor da classe Treinador.
+     *
+     * @param ID O identificador do treinador.
+     * @param nome O nome do treinador.
+     * @param equipe A equipe de monstrinhos do treinador.
+     */
     Treinador(int ID, std::string nome, vector<Monstrinho *> equipe);
+
+    /**
+     * @brief Obtém o identificador do treinador.
+     *
+     * @return O identificador do treinador.
+     */
     int getID();
+
+    /**
+     * @brief Obtém o nome do treinador.
+     *
+     * @return O nome do treinador.
+     */
     std::string getNome();
+
+    /**
+     * @brief Obtém a equipe de monstrinhos do treinador.
+     *
+     * @return A equipe de monstrinhos do treinador.
+     */
     vector<Monstrinho *> getEquipe();
+
+    /**
+     * @brief Verifica se a equipe de monstrinhos do treinador está completa.
+     *
+     * @return true se a equipe estiver completa, false caso contrário.
+     */
     bool verificaEquipe();
+
+    /**
+     * @brief Método virtual para mudar a equipe de monstrinhos do treinador.
+     *
+     * Este método deve ser implementado nas classes derivadas.
+     */
     virtual void mudaEquipe();
+
+    /**
+     * @brief Imprime a equipe de monstrinhos do treinador.
+     */
     void imprimeEquipe();
 };
 
