0197a3d037a04d2ec0607341f1cce7079ec42316
Henrique Dias <henriquejnd@proton.me>
2024-06-14 15:03:48 -0300

    refactor: Melhora lógica de Jogador::mudaEquipe()

--- CPPs/Jogador.cpp
+++ CPPs/Jogador.cpp
@@ -12,22 +12,32 @@ void Jogador::mudaEquipe()
         {
             cout << "O " << equipe[0]->getNome() << " foi derrotado!" << endl;
             cout << "Escolha um monstrinho para substituir o que foi derrotado: " << endl;
-            imprimeEquipe();
-            int escolha;
-            cin >> escolha;
-            if (escolha < 1 || escolha > equipe.size())
-            {
-                cout << "Escolha inválida!" << endl;
-                mudaEquipe();
-            }
-            if (equipe[escolha - 1]->getHP() <= 0)
-            {
-                cout << "Esse monstrinho está desmaiado!" << endl;
-                mudaEquipe();
-            }
-            equipe.push_back(equipe[0]);
-            equipe[0] = equipe[escolha - 1];
-            equipe.erase(equipe.begin() + escolha - 1);
         }
+        if (equipe[0]->getHP() > 0)
+        {
+            cout << "Escolha outro monstrinho: " << endl;
+        }
+
+        imprimeEquipe();
+        int escolha;
+        cin >> escolha;
+        if (escolha < 1 || escolha > equipe.size())
+        {
+            cout << "Escolha inválida!" << endl;
+            mudaEquipe();
+        }
+        if (equipe[escolha - 1]->getHP() <= 0)
+        {
+            cout << "Esse monstrinho está desmaiado!" << endl;
+            mudaEquipe();
+        }
+        if (equipe[escolha - 1] == equipe[0] && equipe[0]->getHP() > 0)
+        {
+            cout << "Esse monstrinho já está em batalha!" << endl;
+            mudaEquipe();
+        }
+        equipe.push_back(equipe[0]);
+        equipe[0] = equipe[escolha - 1];
+        equipe.erase(equipe.begin() + escolha - 1);
     }
 }
