110368c17b0a08e8e0585210e155e3118e01e107
Lucas Zioli <lucas.zioli.04@gmail.com>
2024-06-16 14:39:47 -0300

    fix: muda tipo de erro da escolha

--- ERR/EscolhaError.hpp
+++ ERR/EscolhaError.hpp
@@ -2,7 +2,7 @@
 #define ESCOLHAERRO
 #include <stdexcept>
 
-class EscolhaError : public std::runtime_error{
+class EscolhaError : public std::invalid_argument{
     public:
         EscolhaError(const std::string& message): std::runtime_error(message){};
 };
