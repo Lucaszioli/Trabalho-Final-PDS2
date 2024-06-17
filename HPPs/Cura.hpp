76408561d80d48e108e4232558188a7e13a0f1a9
Lucas Zioli <lucas.zioli.04@gmail.com>
2024-06-17 00:15:19 -0300

    Refactor: Refatora os HPP
    Co-authored-by: Otávio Serafim <otavio.serafim2002@gmail.com>
    Co-authored-by: Matheus Gregor <nuvemnm@users.noreply.github.com>
    Co-authored-by: Malu Lauar <lauar-m@users.noreply.github.com>

--- HPPs/Cura.hpp
+++ HPPs/Cura.hpp
@@ -3,13 +3,40 @@
 
 #include "Item.hpp"
 
+/*
+* @class Cura
+* 
+* @brief Classe que representa o item Cura.
+*
+*/
 class Cura: public Item{
     private:
-        int cura;
+        int cura; ///< A cura do item
     public:
+        /**
+        * @brief Método para pegar o item
+        */
         void pegarItem() override;
+
+        /**
+         * @brief Usa um item selecionado
+         * 
+         * @param Monstro O monstrinho em que o item será utilizado
+         * @return true se o item foi usado com sucesso, false se não foi
+         * 
+         */
         bool usarItem(Monstrinho* monstro) override;
+
+        /**
+        * @brief Define a quantidade de cura do item.
+        * @param cura A quantidade de cura.
+        */
         void setCura(int cura);
+
+        /**
+        * @brief Obtém a quantidade de cura do item.
+        * @return A quantidade de cura.
+        */
         int getCura();
 };
 
--- HPPs/Estamina.hpp
+++ HPPs/Estamina.hpp
@@ -3,13 +3,35 @@
 
 #include "Item.hpp"
 
+/** 
+* @class Estamina
+* 
+* @brief Classe que representa o item Estamina.
+*
+*/
+
 class Estamina : public Item{
     private:
-        int energia;
+        int energia; ///< A energia do item 
     public:
+        /**
+        * @brief Método para pegar o item
+        */
         void pegarItem() override;
+
+
         int getEnergia();
+
+        
         void setEnergia(int energia);
+
+        /**
+         * @brief Usa um item selecionado
+         * 
+         * @param Monstro O monstrinho em que o item será utilizado
+         * @return true se o item foi usado com sucesso, false se não foi
+         * 
+         */
         bool usarItem(Monstrinho* monstro) override;
 };
 
